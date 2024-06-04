e147b2db8cbc8a537b8f465fd98cb47c0c350415
vitkon <vitalkond@gmail.com>
2024-06-04 20:16:55 +0100

    add scheduled parquet upload

--- nebulight-services/src/scheduled_task.py
+++ nebulight-services/src/scheduled_task.py
@@ -1,33 +1,53 @@
-# Import necessary modules
 from sqlalchemy import create_engine, Column, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from datetime import datetime
-import uuid
+import os
+from tempfile import NamedTemporaryFile
+import boto3
+import pandas as pd
+import fastparquet as fp
+from io import BytesIO
+import yfinance as yf
 from .models import Industry
 from .database import engine
 
-Session = sessionmaker(bind=engine)
-session = Session()
+def add_file(event=None, context=None):
+    s3_bucket = os.getenv('S3_DATA_BUCKET')
+    s3_client = boto3.client('s3')
 
+    ticker_symbol1 = 'AAPL' 
+    ticker1 = yf.Ticker(ticker_symbol1)
+    df1 = ticker1.history(period='1d')
+    
+    ticker_symbol2 = 'MSFT' 
+    ticker2 = yf.Ticker(ticker_symbol2)
+    df2 = ticker2.history(period='1d')
+    
+    combined_data = pd.concat([df1, df2])
+    
+    file_name = f"stock-data-metrics-{datetime.now().strftime('%Y%m%d%H%M%S')}.parquet"
 
-def add_industry(name):
-    new_industry = Industry(name=name)
+    # Convert the DataFrame to Parquet format and save it to a temporary file
+    with NamedTemporaryFile(delete=False) as temp_file:
+        combined_data.to_parquet(temp_file.name)
+        temp_file_path = temp_file.name
+
+    # Upload the Parquet file to S3
     try:
-        session.add(new_industry)
-        session.commit()
-        print(f"Industry '{name}' added successfully")
+        with open(temp_file_path, 'rb') as file_data:
+            s3_client.put_object(Bucket=s3_bucket, Key=file_name, Body=file_data)
+        print(f"File '{file_name}' uploaded successfully to bucket '{s3_bucket}'")
     except Exception as e:
-        # session.rollback()
-        print(f"Error adding industry: {e}")
-
-def scheduled_task_handler(event=None, context=None):
+        print(f"Error uploading file to S3: {e}")
+    finally:
+        os.remove(temp_file_path)  # Clean up the temporary file
+def scheduled_task_handler(event, context):
     print('launch scheduled task')
     try:
-        timestamp = datetime.utcnow()
-        add_industry("test " + str(timestamp))
+        print('scheduled task launched')
+        # timestamp = datetime.utcnow()
+        # add_industry("test " + str(timestamp))
+        # add_file2()
     except Exception as e:
         print(f"Error in scheduled task: {e}")
-
-
-scheduled_task_handler()
